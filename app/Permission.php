71f960cb59a15f0978aaa5fcfd8eaa30ad605a44
Robert Andrei Parasca <Robert.Parasca@endava.com>
2019-10-28 11:34:54 +0200

    Gates wtf

---
+++ app/Permission.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    public function user()
+    {
+        return $this->belongsToMany(User::class, 'user_permissions', 'user_id');
+    }
+}
